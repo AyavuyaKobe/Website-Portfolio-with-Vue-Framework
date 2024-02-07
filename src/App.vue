8a0eb15c5795b4e298aa240a25aae0df290435b0
Ayavuya Kobe <141222418+AyavuyaKobe@users.noreply.github.com>
2024-02-07 13:09:25 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <HomePage  />
+</template>
+
+<script>
+import HomePage from "./HomePage/HomePage.vue";
+import "./vars.css";
+import "./styles.css";
+
+export default {
+  name: "App",
+  components: {
+    HomePage: HomePage,
+  },
+};
+</script>
+
+<style>
+</style>
+
